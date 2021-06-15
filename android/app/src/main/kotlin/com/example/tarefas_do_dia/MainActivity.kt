0768ab9eb2451fa1cea3b248438155e03c899ce8
Igor Rabelo <igorrabelof10@gmail.com>
2021-06-15 17:59:14 -0300

    App Pronto

---
+++ android/app/src/main/kotlin/com/example/tarefas_do_dia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarefas_do_dia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tarefas_do_dia/Home.dart';
+
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
